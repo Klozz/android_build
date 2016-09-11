Team-OctOS/platform_build

Add Garbage Collection to mka
Change-Id: I21a30c9f0f6d1b889fa14547734df7ba228bb7d9

envsetup.sh
@@ -1702,6 +1702,13 @@ function installrecovery()
 }
 
 function mka() {
+    ## Clean Up the Garbage
+    echo "Removing Previous Builds for $OUT"
+    rm -rf $OUT/system/build.prop
+    rm -rf $OUT/OCT-L*.zip*
+    rm -rf $OUT/to_*ota*.zip
+    echo "Clean Up Complete! Time to Make it Dirty"
+
     case `uname -s` in
         Darwin)
             make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"

Klozz/android_build

envsetup.sh
@@ -1553,6 +1553,13 @@ function godir () {
 }
 
 function mka() {
+    ## Clean Up the Garbage
+    echo "Removing Previous Builds for $OUT"
+    rm -rf $OUT/system/build.prop
+    rm -rf $OUT/OCT-N*.zip*
+    rm -rf $OUT/to_*ota*.zip
+    echo "Clean Up Complete! Time to Make it Dirty"
+
     case `uname -s` in
         Darwin)
             make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"